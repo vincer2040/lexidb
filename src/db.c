47c8d4df3c7b3da3fc304a29452a21bc7266e37f
boreddad <ronnebaumfinances@gmail.com>
2023-07-13 16:35:15 -0400

    chore: versions and help

--- src/db.c
+++ src/db.c
@@ -1,44 +1,24 @@
-#include "ht.h"
+#include "builder.h"
+#include "server.h"
+#include "util.h"
+#include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void fcb(void* value) { free(value); }
+#define ADDR "127.0.0.1"
+#define PORT 6969
 
 int main(int argc, char** argv) {
-    Ht* ht;
+    Builder builder = { 0 };
 
-    void *t1, *t2;
-    if (argc > 1) {
-        int i;
-        for (i = 1; i < argc; ++i) {
-            printf("%s\n", argv[i]);
-        }
+    if (done_from_args(argc, argv) == 1) {
+        return 0;
     }
 
-    ht = ht_new(32);
-    if (ht == NULL) {
-        return 1;
-    }
+    new_builder(&builder, 32);
 
-    ht_insert(ht, ((uint8_t*)"dom"), 3, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"madi"), 4, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"mom"), 3, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"vince"), 5, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"dad"), 3, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"frank"), 5, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"lomein"), 7, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"kitty"), 5, "is cool", 8, fcb);
-    ht_insert(ht, ((uint8_t*)"boller"), 6, "is cool", 8, fcb);
-
-    t1 = ht_get(ht, ((uint8_t*)"vince"), 5);
-    printf("%s\n", ((char*)t1));
-    ht_delete(ht, ((uint8_t*)"vince"), 5);
-    t2 = ht_get(ht, ((uint8_t*)"vince"), 5);
-    printf("%p\n", t2);
-
-    ht_print(ht);
-
-    ht_free(ht);
     return 0;
 }
---
+++ src/db.h
@@ -0,0 +1,13 @@
+#ifndef __DB_H__
+
+#define __DB_H__
+
+#define VERSION "0.1"
+
+#define HELP "\
+Options\n\
+    --version (-v)      = Print the current version\n\
+    --help (-h)         = Print this information\n\
+"
+
+#endif
