968f6479b155302293e1ecfe2dd760789a9d4999
boreddad <ronnebaumfinances@gmail.com>
2023-07-16 08:56:09 -0400

    test: parser errors

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -39,11 +39,124 @@ void t1() {
     assert(cmd0.expression.get.key.len == 5);
 
     cmdir_free(&cmd_ir);
-    printf("parser test 1 passed\n");
+    parser_free_errors(&p);
+    printf("parser test 1 passed (it works)\n");
+}
+
+void t2() {
+    Lexer l;
+    Parser p;
+    CmdIR cmd_ir;
+    size_t e_len;
+
+    parser_toggle_debug(0);
+
+    uint8_t* input =
+        ((uint8_t*)"*2\n*3\r\n$3\r\nSET\r\n$5\r\nvince\r\n$7\r\nis "
+                   "cool\r\n*2\r\n$3\r\nGET\r\n$5\r\nvince\r\n");
+    size_t inp_len = strlen(((char*)input));
+
+    l = lexer_new(input, inp_len);
+    p = parser_new(&l);
+
+    cmd_ir = parse_cmd(&p);
+
+    e_len = parser_errors_len(&p);
+
+    assert(e_len == 1);
+
+    cmdir_free(&cmd_ir);
+    parser_free_errors(&p);
+    printf("parser test 2 passed (arr missing retcar after len)\n");
+}
+
+void t3() {
+    Lexer l;
+    Parser p;
+    CmdIR cmd_ir;
+    size_t e_len;
+
+    parser_toggle_debug(0);
+
+    uint8_t* input =
+        ((uint8_t*)"*2\r*3\r\n$3\r\nSET\r\n$5\r\nvince\r\n$7\r\nis "
+                   "cool\r\n*2\r\n$3\r\nGET\r\n$5\r\nvince\r\n");
+    size_t inp_len = strlen(((char*)input));
+
+    l = lexer_new(input, inp_len);
+    p = parser_new(&l);
+
+    cmd_ir = parse_cmd(&p);
+
+    e_len = parser_errors_len(&p);
+
+    assert(e_len == 1);
+
+    cmdir_free(&cmd_ir);
+    parser_free_errors(&p);
+    printf("parser test 3 passed (arr missing newl after len)\n");
+}
+
+void t4() {
+    Lexer l;
+    Parser p;
+    CmdIR cmd_ir;
+    size_t e_len;
+
+    parser_toggle_debug(0);
+
+    uint8_t* input =
+        ((uint8_t*)"*\r\n*3\r\n$3\r\nSET\r\n$5\r\nvince\r\n$7\r\nis "
+                   "cool\r\n*2\r\n$3\r\nGET\r\n$5\r\nvince\r\n");
+    size_t inp_len = strlen(((char*)input));
+
+    l = lexer_new(input, inp_len);
+    p = parser_new(&l);
+
+    cmd_ir = parse_cmd(&p);
+
+    e_len = parser_errors_len(&p);
+
+    assert(e_len == 1);
+
+    cmdir_free(&cmd_ir);
+    parser_free_errors(&p);
+    printf("parser test 4 passed (arr missing len)\n");
+}
+
+void t5() {
+    Lexer l;
+    Parser p;
+    CmdIR cmd_ir;
+    size_t e_len;
+
+    parser_toggle_debug(0);
+
+    uint8_t* input =
+        ((uint8_t*)"3\r\n*3\r\n$3\r\nSET\r\n$5\r\nvince\r\n$7\r\nis "
+                   "cool\r\n*2\r\n$3\r\nGET\r\n$5\r\nvince\r\n");
+    size_t inp_len = strlen(((char*)input));
+
+    l = lexer_new(input, inp_len);
+    p = parser_new(&l);
+
+    cmd_ir = parse_cmd(&p);
+
+    e_len = parser_errors_len(&p);
+
+    assert(e_len == 1);
+
+    cmdir_free(&cmd_ir);
+    parser_free_errors(&p);
+    printf("parser test 5 passed (missing arr type)\n");
 }
 
 int main(void) {
     t1();
+    t2();
+    t3();
+    t4();
+    t5();
     printf("all parser tests passed\n");
     return 0;
 }
