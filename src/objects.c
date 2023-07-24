0664dc5c3389ebf4a23856838187bc9f3233cf9d
boreddad <ronnebaumfinances@gmail.com>
2023-07-23 23:27:10 -0400

    feat: vector

--- src/objects.c
+++ src/objects.c
@@ -90,3 +90,78 @@ void string_free(String* str) {
     free(str);
     str = NULL;
 }
+
+Vec* vec_new(size_t initial_cap, size_t data_size) {
+    Vec* vec;
+    size_t needed_size;
+
+    needed_size = sizeof(Vec) + (data_size * initial_cap);
+
+    vec = malloc(needed_size);
+    if (vec == NULL) {
+        return NULL;
+    }
+
+    memset(vec, 0, needed_size);
+
+    vec->data_size = data_size;
+    vec->cap = initial_cap;
+
+    return vec;
+}
+
+int vec_push(Vec** vec, void* data) {
+    size_t len, cap, size, len_x_size;
+    len = (*vec)->len;
+    cap = (*vec)->cap;
+    size = (*vec)->data_size;
+    len_x_size = len * size;
+
+    if (len == cap) {
+        size_t cap_x_size, needed_size;
+        cap <<= 1;
+        cap_x_size = cap * size;
+        needed_size = sizeof(Vec) + cap_x_size;
+        *vec = realloc(*vec, needed_size);
+        if (*vec == NULL) {
+            return -1;
+        }
+
+        memset((*vec)->data + len_x_size, 0, cap_x_size - len_x_size);
+
+        (*vec)->cap = cap;
+    }
+
+    memcpy((*vec)->data + len_x_size, data, size);
+
+    (*vec)->len++;
+
+    return 0;
+}
+
+void vec_for_each(Vec* vec, VecForEach* fn) {
+    size_t i, len, size;
+    len = vec->len;
+    size = vec->data_size;
+
+    for (i = 0; i < len; ++i) {
+        void* item = vec->data + (i * size);
+        fn(item);
+    }
+
+}
+
+void vec_free(Vec* vec, VecFreeCallBack* cb) {
+    size_t i, len, size;
+    len = vec->len;
+    size = vec->data_size;
+
+    if (cb) {
+        for (i = 0; i < len; ++i) {
+            void* item = vec->data + (i * size);
+            cb(item);
+        }
+    }
+
+    free(vec);
+}
--- src/objects.h
+++ src/objects.h
@@ -11,6 +11,16 @@ typedef struct {
     char data[];
 } String;
 
+typedef void VecFreeCallBack(void* ptr);
+typedef void VecForEach(void* ptr);
+
+typedef struct {
+    size_t len;
+    size_t cap;
+    size_t data_size;
+    unsigned char data[];
+} Vec;
+
 typedef enum {
     ONULL,
     OINT,
@@ -28,12 +38,21 @@ typedef struct {
     ObjectD data;
 } Object;
 
+/* generic object */
 Object object_new(ObjectT type, void* v, size_t val_len);
 void object_free(Object* obj);
+
+/* string */
 String* string_new(size_t initial_cap);
 String* string_from(char* value, size_t len);
 void string_print(String* str);
 int string_push_char(String** str, char c);
 void string_free(String* str);
+
+/* vector */
+Vec* vec_new(size_t initial_cap, size_t data_size);
+int vec_push(Vec** vec, void* data);
+void vec_for_each(Vec* vec, VecForEach* fn);
+void vec_free(Vec* vec, VecFreeCallBack* cb);
 
 #endif
