b43aee3fcf72f9aab25b0c2181ac4ee1122d9327
boreddad <ronnebaumfinances@gmail.com>
2023-10-12 11:00:04 -0400

    chore: rewrite builder properly

--- src/builder.c
+++ src/builder.c
@@ -17,186 +17,172 @@ Builder builder_create(size_t initial_cap) {
     return builder;
 }
 
+static int builder_realloc(Builder* builder, size_t needed) {
+    size_t new_cap = builder->cap + needed;
+    void* tmp = realloc(builder->buf, new_cap);
+    if (tmp == NULL) {
+        return -1;
+    }
+    builder->buf = tmp;
+    builder->cap = new_cap;
+    memset(builder->buf + builder->ins, 0, new_cap - builder->ins);
+    return 0;
+}
+
+static inline void builder_add_type_byte(Builder* builder, uint8_t byte) {
+    builder->buf[builder->ins] = byte;
+    builder->ins += 1;
+}
+
+static inline void builder_add_byte(Builder* builder, uint8_t byte) {
+    builder->buf[builder->ins] = byte;
+    builder->ins += 1;
+}
+
+static inline void builder_add_str(Builder* builder, uint8_t* str, size_t len) {
+    memcpy(builder->buf + builder->ins, str, len);
+    builder->ins += len;
+}
+
+static inline void builder_add_end(Builder* builder) {
+    builder->buf[builder->ins] = '\r';
+    builder->ins += 1;
+    builder->buf[builder->ins] = '\n';
+    builder->ins += 1;
+}
+
+static inline void builder_add_len(Builder* builder, size_t len) {
+    size_t len_buf_len;
+    char len_buf[20] = {0};
+    sprintf(len_buf, "%lu", len);
+    len_buf_len = strlen(len_buf);
+    memcpy(builder->buf + builder->ins, len_buf, len_buf_len);
+    builder->ins += len_buf_len;
+}
+
 int builder_add_pong(Builder* builder) {
-    if (builder->cap < 7) {
-        void* tmp;
-        tmp = realloc(builder->buf, sizeof(uint8_t) * 7);
-        if (tmp == NULL) {
+    size_t needed = builder->ins + 7;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf, 0, 7);
     }
-    memcpy(builder->buf, "+PONG\r\n", 7);
-    builder->ins = 7;
+    builder_add_type_byte(builder, '+');
+    builder_add_str(builder, (uint8_t*)"PONG", 4);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_ping(Builder* builder) {
-    if (builder->cap < 7) {
-        void* tmp;
-        tmp = realloc(builder->buf, sizeof(uint8_t) * 7);
-        if (tmp == NULL) {
+    size_t needed = builder->ins + 7;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf, 0, 7);
     }
-    memcpy(builder->buf, "+PING\r\n", 7);
-    builder->ins = 7;
+    builder_add_type_byte(builder, '+');
+    builder_add_str(builder, (uint8_t*)"PING", 4);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_ok(Builder* builder) {
-    if (builder->cap < 5) {
-        void* tmp;
-        tmp = realloc(builder->buf, sizeof(uint8_t) * 5);
-        if (tmp == NULL) {
+    size_t needed = builder->ins + 5;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf, 0, 5);
     }
-    memcpy(builder->buf, "+OK\r\n", 5);
-    builder->ins = 5;
+    builder_add_type_byte(builder, '+');
+    builder_add_str(builder, (uint8_t*)"OK", 2);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_none(Builder* builder) {
-    if (builder->cap < 7) {
-        void* tmp;
-        tmp = realloc(builder->buf, sizeof(uint8_t) * 7);
-        if (tmp == NULL) {
+    size_t needed = builder->ins + 7;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf, 0, 7);
     }
-    memcpy(builder->buf, "+NONE\r\n", 7);
-    builder->ins = 7;
+    builder_add_type_byte(builder, '+');
+    builder_add_str(builder, (uint8_t*)"NONE", 4);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_err(Builder* builder, uint8_t* e, size_t e_len) {
-    size_t needed_len = 3 + e_len;
-    if (builder->cap < needed_len) {
-        void* tmp;
-        tmp = realloc(builder->buf, sizeof(uint8_t) * needed_len);
-        if (tmp == NULL) {
+    size_t needed = builder->ins + 3 + e_len;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf, 0, needed_len);
     }
-    builder->buf[0] = ((uint8_t)'-');
-    memcpy(builder->buf + 1, e, e_len);
-    builder->buf[e_len + 1] = '\r';
-    builder->buf[e_len + 2] = '\n';
-    builder->ins = needed_len;
+    builder_add_type_byte(builder, '-');
+    builder_add_str(builder, e, e_len);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_arr(Builder* builder, size_t arr_len) {
-    size_t needed_len;
-    size_t str_len_buf_len;
-    char str_len_buf[20] = {0};
-    sprintf(str_len_buf, "%lu", arr_len);
-    str_len_buf_len = strlen(str_len_buf);
-
-    needed_len = builder->ins + str_len_buf_len + 3;
-
-    if (needed_len > builder->cap) {
-        void* tmp;
-        builder->cap = needed_len + 120;
-        tmp = realloc(builder->buf, (sizeof(uint8_t)) * builder->cap);
-        if (tmp == NULL) {
-            builder->cap = 0;
-            builder->ins = 0;
+    size_t needed = builder->ins + 21;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf + builder->ins, 0, builder->cap - builder->ins);
     }
-    builder->buf[builder->ins] = '*';
-    builder->ins++;
-    memcpy(builder->buf + builder->ins, str_len_buf, str_len_buf_len);
-    builder->ins += str_len_buf_len;
-    builder->buf[builder->ins] = '\r';
-    builder->ins++;
-    builder->buf[builder->ins] = '\n';
-    builder->ins++;
+    builder_add_type_byte(builder, '*');
+    builder_add_len(builder, arr_len);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_string(Builder* builder, char* str, size_t str_len) {
-    size_t needed_len;
-    size_t str_len_buf_len;
-    char str_len_buf[20] = {0};
-    sprintf(str_len_buf, "%lu", str_len);
-    str_len_buf_len = strlen(str_len_buf);
-
-    needed_len = builder->ins + str_len_buf_len + 5 + str_len;
-
-    if (needed_len > builder->cap) {
-        void* tmp;
-        builder->cap = needed_len + 120;
-        tmp = realloc(builder->buf, (sizeof(uint8_t)) * builder->cap);
-        if (tmp == NULL) {
-            builder->cap = 0;
-            builder->ins = 0;
+    size_t needed = builder->ins + str_len + 20 + 5;
+    size_t cap = builder->cap;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf + builder->ins, 0, builder->cap - builder->ins);
     }
-    builder->buf[builder->ins] = '$';
-    builder->ins++;
-    memcpy(builder->buf + builder->ins, str_len_buf, str_len_buf_len);
-    builder->ins += str_len_buf_len;
-    builder->buf[builder->ins] = '\r';
-    builder->ins++;
-    builder->buf[builder->ins] = '\n';
-    builder->ins++;
-    memcpy(builder->buf + builder->ins, str, str_len);
-    builder->ins += str_len;
-    builder->buf[builder->ins] = '\r';
-    builder->ins++;
-    builder->buf[builder->ins] = '\n';
-    builder->ins++;
+    builder_add_type_byte(builder, '$');
+    builder_add_len(builder, str_len);
+    builder_add_end(builder);
+    builder_add_str(builder, (uint8_t*)str, str_len);
+    builder_add_end(builder);
     return 0;
 }
 
 int builder_add_int(Builder* builder, int64_t val) {
-    size_t needed_len = builder->ins + sizeof(int64_t) + 3;
+    size_t needed = builder->ins + 11;
     size_t cap = builder->cap;
-    size_t len = builder->ins;
-    uint8_t shift = 56;
-    size_t i;
-
-    if (needed_len > cap) {
-        cap += needed_len + 32;
-        void* tmp;
-        tmp = realloc(builder->buf, sizeof(uint8_t) * cap);
-        if (tmp == NULL) {
+    size_t i, len = 8, shift = 56;
+    if (cap < needed) {
+        needed <<= 1;
+        if (builder_realloc(builder, needed) == -1) {
             return -1;
         }
-        builder->buf = tmp;
-        memset(builder->buf + len, 0, cap - len);
-        builder->cap = cap;
     }
-
-    builder->buf[len] = ':';
-    len++;
-
-    for (i = 0; i < 8; ++i, ++len, shift-=8) {
-        builder->buf[len] = val >> shift;
+    builder_add_type_byte(builder, ':');
+    for (i = 0; i < len; ++i, shift -= 8) {
+        uint8_t at = val >> shift;
+        builder_add_byte(builder, at);
     }
-
-    builder->buf[len] = '\r';
-    len++;
-    builder->buf[len] = '\n';
-
-    builder->ins += 11;
+    builder_add_end(builder);
     return 0;
 }
 
