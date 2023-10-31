93986b1e9627d84c3db68c32c4adcfec34a2b76e
boreddad <ronnebaumfinances@gmail.com>
2023-10-31 14:01:33 -0400

    chore: remove unused functions

--- src/builder.h
+++ src/builder.h
@@ -32,6 +32,5 @@ int builder_add_string(Builder* builder, char* str, size_t str_len);
 int builder_add_int(Builder* builder, int64_t val);
 uint8_t* builder_out(Builder* builder);
 void builder_reset(Builder* builder);
-void builder_print(Builder* builder);
 
 #endif
