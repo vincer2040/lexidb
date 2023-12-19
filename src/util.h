8d3bb80c34dcf22c36d57ba9762b68d28886bfd4
boreddad <ronnebaumfinances@gmail.com>
2023-12-18 20:26:20 -0500

    feat: generic result type preproc macro

--- src/util.h
+++ src/util.h
@@ -11,4 +11,22 @@ typedef void free_fn(void* ptr);
 
 void get_random_bytes(uint8_t* p, size_t len);
 
+struct timespec get_time(void);
+
+typedef enum {
+    Ok,
+    Err,
+} result_type;
+
+#define result_t(val_type, err_type)                                           \
+    typedef struct {                                                           \
+        result_type type;                                                      \
+        union {                                                                \
+            val_type ok;                                                       \
+            err_type err;                                                      \
+        } data;                                                                \
+    } result_##val_type
+
+#define result(val_type) result_##val_type
+
 #endif /* __UTIL_H__ */
