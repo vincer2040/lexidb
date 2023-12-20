be29cb7045ff3434cf9de5f5092c0bfb42836237
boreddad <ronnebaumfinances@gmail.com>
2023-12-20 12:51:18 -0500

    feat: basic logging library

---
+++ src/log.h
@@ -0,0 +1,54 @@
+#ifndef __LOG_H__
+
+#define __LOG_H__
+
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define info(...)                                                              \
+    do {                                                                       \
+        struct timespec time = get_time();                                     \
+        struct tm* tm = localtime(&time.tv_sec);                               \
+        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+               tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
+        printf("\033[32m INFO  \033[39m");                                     \
+        printf(__VA_ARGS__);                                                   \
+        fflush(stdout);                                                        \
+    } while (0)
+
+#define warn(...)                                                              \
+    do {                                                                       \
+        struct timespec time = get_time();                                     \
+        struct tm* tm = localtime(&time.tv_sec);                               \
+        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+               tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
+        printf("\033[33m WARN  \033[39m");                                     \
+        printf(__VA_ARGS__);                                                   \
+        fflush(stdout);                                                        \
+    } while (0)
+
+#define error(...)                                                             \
+    do {                                                                       \
+        struct timespec time = get_time();                                     \
+        struct tm* tm = localtime(&time.tv_sec);                               \
+        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+               tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
+        printf("\033[31m ERROR \033[39m");                                     \
+        printf(__VA_ARGS__);                                                   \
+        fflush(stdout);                                                        \
+    } while (0)
+
+#define debug(...)                                                             \
+    do {                                                                       \
+        struct timespec time = get_time();                                     \
+        struct tm* tm = localtime(&time.tv_sec);                               \
+        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+               tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
+        printf("\033[34m DEBUG \033[39m");                                     \
+        printf("%s:%d ", __FILE__, __LINE__);                                  \
+        printf(__VA_ARGS__);                                                   \
+        fflush(stdout);                                                        \
+    } while (0)
+
+#endif /* __LOG_H__ */
