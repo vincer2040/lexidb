19c9eb649719741964b0cba30ccd82162489ac62
boreddad <ronnebaumfinances@gmail.com>
2023-12-20 17:02:33 -0500

    feat: add padding to date numbers

--- src/log.h
+++ src/log.h
@@ -10,7 +10,7 @@
     do {                                                                       \
         struct timespec time = get_time();                                     \
         struct tm* tm = localtime(&time.tv_sec);                               \
-        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+        printf("%d-%d-%d %02d:%02d:%02d", tm->tm_mon + 1, tm->tm_mday,         \
                tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
         printf("\033[32m INFO  \033[39m");                                     \
         printf(__VA_ARGS__);                                                   \
@@ -21,7 +21,7 @@
     do {                                                                       \
         struct timespec time = get_time();                                     \
         struct tm* tm = localtime(&time.tv_sec);                               \
-        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+        printf("%d-%d-%d %02d:%02d:%02d", tm->tm_mon + 1, tm->tm_mday,         \
                tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
         printf("\033[33m WARN  \033[39m");                                     \
         printf(__VA_ARGS__);                                                   \
@@ -32,8 +32,8 @@
     do {                                                                       \
         struct timespec time = get_time();                                     \
         struct tm* tm = localtime(&time.tv_sec);                               \
-        fprintf(stderr, "%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,          \
-                tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);      \
+        printf("%d-%d-%d %02d:%02d:%02d", tm->tm_mon + 1, tm->tm_mday,         \
+               tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
         fprintf(stderr, "\033[31m ERROR \033[39m");                            \
         fprintf(stderr, __VA_ARGS__);                                          \
         fflush(stderr);                                                        \
@@ -43,7 +43,7 @@
     do {                                                                       \
         struct timespec time = get_time();                                     \
         struct tm* tm = localtime(&time.tv_sec);                               \
-        printf("%d-%d-%d %d:%d:%d", tm->tm_mon, tm->tm_mday,                   \
+        printf("%d-%d-%d %02d:%02d:%02d", tm->tm_mon + 1, tm->tm_mday,         \
                tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);       \
         printf("\033[34m DEBUG \033[39m");                                     \
         printf("%s:%d ", __FILE__, __LINE__);                                  \
