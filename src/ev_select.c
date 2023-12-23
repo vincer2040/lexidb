8a1f1e74335b12f7454c306f821b2179fc74f451
boreddad <ronnebaumfinances@gmail.com>
2023-12-23 13:33:54 -0500

    chore: rename some variables

--- src/ev_select.c
+++ src/ev_select.c
@@ -14,7 +14,7 @@ typedef struct ev_api {
     fd_set _wfds;
 } ev_api;
 
-static int ev_api_new(ev* eventLoop) {
+static int ev_api_new(ev* ev) {
     ev_api* state = calloc(1, sizeof(ev_api));
 
     if (!state) {
@@ -22,22 +22,22 @@ static int ev_api_new(ev* eventLoop) {
     }
     FD_ZERO(&state->rfds);
     FD_ZERO(&state->wfds);
-    eventLoop->api = state;
+    ev->api = state;
     return 0;
 }
 
-static int ev_api_resize(ev* eventLoop, int setsize) {
-    ev_unused(eventLoop);
+static int ev_api_resize(ev* ev, int setsize) {
+    ev_unused(ev);
     /* Just ensure we have enough room in the fd_set type. */
     if (setsize >= FD_SETSIZE)
         return -1;
     return 0;
 }
 
-static void ev_api_free(ev* eventLoop) { free(eventLoop->api); }
+static void ev_api_free(ev* ev) { free(ev->api); }
 
-static int ev_api_add_event(ev* eventLoop, int fd, int mask) {
-    ev_api* state = eventLoop->api;
+static int ev_api_add_event(ev* ev, int fd, int mask) {
+    ev_api* state = ev->api;
 
     if (mask & EV_READ) {
         FD_SET(fd, &state->rfds);
@@ -48,8 +48,8 @@ static int ev_api_add_event(ev* eventLoop, int fd, int mask) {
     return 0;
 }
 
-static void ev_api_del_event(ev* eventLoop, int fd, int mask) {
-    ev_api* state = eventLoop->api;
+static void ev_api_del_event(ev* ev, int fd, int mask) {
+    ev_api* state = ev->api;
     if (mask & EV_READ) {
         FD_CLR(fd, &state->rfds);
     }
@@ -58,19 +58,18 @@ static void ev_api_del_event(ev* eventLoop, int fd, int mask) {
     }
 }
 
-static int ev_api_poll(ev* eventLoop, struct timeval* tvp) {
-    ev_api* state = eventLoop->api;
+static int ev_api_poll(ev* ev, struct timeval* tvp) {
+    ev_api* state = ev->api;
     int retval, j, numevents = 0;
 
     memcpy(&state->_rfds, &state->rfds, sizeof(fd_set));
     memcpy(&state->_wfds, &state->wfds, sizeof(fd_set));
 
-    retval =
-        select(eventLoop->max_fd + 1, &state->_rfds, &state->_wfds, NULL, tvp);
+    retval = select(ev->max_fd + 1, &state->_rfds, &state->_wfds, NULL, tvp);
     if (retval > 0) {
-        for (j = 0; j <= eventLoop->max_fd; j++) {
+        for (j = 0; j <= ev->max_fd; j++) {
             int mask = 0;
-            ev_file_event* fe = &eventLoop->events[j];
+            ev_file_event* fe = &ev->events[j];
 
             if (fe->mask == EV_NONE) {
                 continue;
@@ -81,8 +80,8 @@ static int ev_api_poll(ev* eventLoop, struct timeval* tvp) {
             if (fe->mask & EV_WRITE && FD_ISSET(j, &state->_wfds)) {
                 mask |= EV_WRITE;
             }
-            eventLoop->fired[numevents].fd = j;
-            eventLoop->fired[numevents].mask = mask;
+            ev->fired[numevents].fd = j;
+            ev->fired[numevents].mask = mask;
             numevents++;
         }
     } else if (retval == -1 && errno != EINTR) {
