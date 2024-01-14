586922d08e6f3e23b6f2eead5fd670369f7646d6
boreddad <ronnebaumfinances@gmail.com>
2024-01-14 13:25:46 -0500

    chore: separate cmd execution into another function

--- src/lexi-cli.c
+++ src/lexi-cli.c
@@ -12,6 +12,8 @@
 
 #define PROMPT "lexi> "
 
+static result(object) execute_cmd(hilexi* l, cmd* cmd);
+
 int main(void) {
     result(hilexi) rl = hilexi_new("127.0.0.1", 6969);
     hilexi l;
@@ -62,74 +64,10 @@ int main(void) {
             break;
         }
 
-        // TODO: add some sort of help command
-
         cmd = parse_line(line_data, line_len);
-        switch (cmd.type) {
-        case Ping:
-            cmd_res = hilexi_ping(&l);
-            break;
-        case Infoc:
-            cmd_res = hilexi_info(&l);
-            break;
-        case Set: {
-            set_cmd set = cmd.data.set;
-            object key = set.key;
-            object value = set.value;
-            cmd_res = hilexi_set(&l, &key, &value);
-            object_free(&key);
-            object_free(&value);
-        } break;
-        case Get: {
-            get_cmd get = cmd.data.get;
-            object key = get.key;
-            cmd_res = hilexi_get(&l, &key);
-            object_free(&key);
-        } break;
-        case Del: {
-            del_cmd del = cmd.data.del;
-            object key = del.key;
-            cmd_res = hilexi_del(&l, &key);
-            object_free(&key);
-        } break;
-        case Push: {
-            push_cmd push = cmd.data.push;
-            object value = push.value;
-            cmd_res = hilexi_push(&l, &value);
-            object_free(&value);
-        } break;
-        case Pop:
-            cmd_res = hilexi_pop(&l);
-            break;
-        case Enque: {
-            enque_cmd enque = cmd.data.enque;
-            object value = enque.value;
-            cmd_res = hilexi_enque(&l, &value);
-            object_free(&value);
-        } break;
-        case Deque:
-            cmd_res = hilexi_deque(&l);
-            break;
-        case ZSet: {
-            object value = cmd.data.zset.value;
-            cmd_res = hilexi_zset(&l, &value);
-            object_free(&value);
-        } break;
-        case ZHas: {
-            object value = cmd.data.zhas.value;
-            cmd_res = hilexi_zhas(&l, &value);
-            object_free(&value);
-        } break;
-        case ZDel: {
-            object value = cmd.data.zdel.value;
-            cmd_res = hilexi_zdel(&l, &value);
-            object_free(&value);
-        } break;
-        default:
-            cmd_res.type = Err;
-            cmd_res.data.err = vstr_from("invalid command");
-            break;
-        }
+
+        cmd_res = execute_cmd(&l, &cmd);
+
         vstr_free(&line);
         if (cmd_res.type == Err) {
             error("%s\n", vstr_data(&(cmd_res.data.err)));
@@ -143,4 +81,79 @@ int main(void) {
     }
     hilexi_close(&l);
     return 0;
+}
+
+static result(object) execute_cmd(hilexi* l, cmd* cmd) {
+    result(object) cmd_res = {0};
+    switch (cmd->type) {
+    case Ping:
+        cmd_res = hilexi_ping(l);
+        break;
+    case Infoc:
+        cmd_res = hilexi_info(l);
+        break;
+    case Help: {
+        vstr h = vstr_from("help");
+        cmd_res.type = Ok;
+        cmd_res.data.ok = object_new(String, &h);
+    } break;
+    case Set: {
+        set_cmd set = cmd->data.set;
+        object key = set.key;
+        object value = set.value;
+        cmd_res = hilexi_set(l, &key, &value);
+        object_free(&key);
+        object_free(&value);
+    } break;
+    case Get: {
+        get_cmd get = cmd->data.get;
+        object key = get.key;
+        cmd_res = hilexi_get(l, &key);
+        object_free(&key);
+    } break;
+    case Del: {
+        del_cmd del = cmd->data.del;
+        object key = del.key;
+        cmd_res = hilexi_del(l, &key);
+        object_free(&key);
+    } break;
+    case Push: {
+        push_cmd push = cmd->data.push;
+        object value = push.value;
+        cmd_res = hilexi_push(l, &value);
+        object_free(&value);
+    } break;
+    case Pop:
+        cmd_res = hilexi_pop(l);
+        break;
+    case Enque: {
+        enque_cmd enque = cmd->data.enque;
+        object value = enque.value;
+        cmd_res = hilexi_enque(l, &value);
+        object_free(&value);
+    } break;
+    case Deque:
+        cmd_res = hilexi_deque(l);
+        break;
+    case ZSet: {
+        object value = cmd->data.zset.value;
+        cmd_res = hilexi_zset(l, &value);
+        object_free(&value);
+    } break;
+    case ZHas: {
+        object value = cmd->data.zhas.value;
+        cmd_res = hilexi_zhas(l, &value);
+        object_free(&value);
+    } break;
+    case ZDel: {
+        object value = cmd->data.zdel.value;
+        cmd_res = hilexi_zdel(l, &value);
+        object_free(&value);
+    } break;
+    default:
+        cmd_res.type = Err;
+        cmd_res.data.err = vstr_from("invalid command");
+        break;
+    }
+    return cmd_res;
 }
